9f39820d42a87fc8344fbe2b821cdf4498d726f2
KOKO Ambinintsoa Kantisambatra <45491492+kokokanti98@users.noreply.github.com>
2021-03-30 16:12:45 +0200

    Corrige des bugs sur les components html du personne et du car

--- src/app/card-detail/car.ts
+++ src/app/card-detail/car.ts
@@ -2,5 +2,10 @@ export class Car {
   plateNumber: string | undefined;
   brand: string | undefined;
   price: number | undefined;
+  numberofseats: number | undefined;
+  maxweight: number | undefined;
+  rents!: Array<{ id: number; beginRent: any; endRent: any; }>;
+  //rented: boolean | undefined;
+  
   //rented: boolean | undefined;
 }
